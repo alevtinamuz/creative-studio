57ad562bf2f078d408a78ebfa7c5a94e4740c288
Pomidorchics <aleksandra180504@gmail.com>
2024-04-15 18:41:12 +1000

    search canvases by user

--- backend/canvas/urls.py
+++ backend/canvas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', api.canvas_list, name='canvas_list'),
     path('feed/<str:id>/', api.canvas_list_feed, name='canvas_list_feed'),
     path('create/', api.canvas_create, name='canvas_create'),
+    path('search_canvas_by_user/', api.search_canvas_by_user, name='search_canvas_by_user'),
+
 ]
